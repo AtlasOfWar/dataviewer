5ceee4499c0dfe6e863d187c8e600802c82382f5
matheus01301 <matheus01301@gmail.com>
2022-01-12 19:59:34 -0300

    oi

---
+++ container-front/src/components/Ranking/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component<props, state> {
+
+class Welcome extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+  }
+}
+s
